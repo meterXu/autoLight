765a9f2e90c388bb3a5d5cb202490e65c3bd62b0
isaac <xhgainxq@qq.com>
2021-05-23 12:37:20 +0800

    ....

--- index.js
+++ index.js
@@ -4,15 +4,26 @@ const schedule = require("node-schedule");
 const y = require("yeelight-awesome");
 const yeelight = new y.Yeelight({...config.yeelight});
 
-schedule.scheduleJob("*/3 * * * * *",async function () {
-    const isOnline = await checkOnLine()
-    await controlLight(isOnline)
-});
+let my,lightStatus = null
+const  duration = 2000, brightness = 29
 
-async function checkOnLine(){
-    for (let host of  config.watchIP){
-        const res =await ping.promise.probe(host,{timeout:10,extra: ['-i', '2'],})
-        if(res.alive){
+yeelight.connect().then(async res => {
+    my = res
+    const pro = await my.getProperty([y.DevicePropery.BRIGHT, y.DevicePropery.POWER])
+    lightStatus = pro.result.result[1] ==='on'
+    schedule.scheduleJob("*/1 * * * * *", async function () {
+        const isOnline = await checkOnLine()
+        console.log(new Date().toLocaleString())
+        console.log(`设备在线状况：${isOnline}`)
+        await controlLight(isOnline)
+    });
+})
+
+
+async function checkOnLine() {
+    for (let host of config.watchIP) {
+        const res = await ping.promise.probe(host, {timeout: 1, extra: ['-i', '2'],})
+        if (res.alive) {
             return Promise.resolve(true)
         }
     }
@@ -21,20 +32,21 @@ async function checkOnLine(){
 
 async function controlLight(status) {
     try {
-        const my = await yeelight.connect()
-        const pro = await my.getProperty([y.DevicePropery.BRIGHT, y.DevicePropery.POWER])
-        if (pro.result.result[1] === (status ? 'off' : 'on')) {
-            await my.toggle()
-            await my.disconnect()
-            let log = `${status ? '开灯成功' : '关灯成功'}`
-            console.log(new Date().toLocaleString())
-            console.log(log)
-        } else {
-            let log = `${status ? '灯已打开' : '灯已关闭'}`
-            console.log(new Date().toLocaleString())
-            console.log(log)
+        if (!my || !my.connected) {
+            my = await yeelight.connect()
+        }
+        if(status !== lightStatus)
+        {
+            const pro = await my.getProperty([y.DevicePropery.BRIGHT, y.DevicePropery.POWER])
+            if (pro.result.result[1] === (status ? 'off' : 'on')) {
+                await my.toggle()
+                let log = `${status ? '开灯成功' : '关灯成功'}`
+                lightStatus = status
+                console.log(new Date().toLocaleString())
+                console.log(log)
+            }
         }
     } catch (err) {
-        console.error(err.message)
+        console.error(err)
     }
 }
